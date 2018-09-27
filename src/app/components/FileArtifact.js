21ab3cf7509c95551cbcc0c6bc8a634b6211395e
Maxim Erekhinskiy <maxim.erehinskiy@jetbrains.com>
2018-09-27 17:15:34 +0300

    Get rid of pretty-bytes lib, fix build

--- src/app/components/FileArtifact.js
+++ src/app/components/FileArtifact.js
@@ -2,12 +2,23 @@ import React from 'react';
 import connect from 'react-redux/es/connect/connect';
 
 import PropTypes from 'prop-types';
-import prettyBytes from 'pretty-bytes';
 
 import {FileIcon} from '@jetbrains/ring-ui/components/icon';
 import Link from '@jetbrains/ring-ui/components/link/link';
 
 import styles from '../app.css';
+
+const fileSize = bytes => {
+  if (bytes === 0) {
+    return '0 Bytes';
+  }
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(2))} ${sizes[i]}`; //eslint-disable-line no-magic-numbers
+};
 
 class FileArtifact extends React.Component {
   static propTypes = {
@@ -23,7 +34,7 @@ class FileArtifact extends React.Component {
         <span className={styles.fileSpacing}/>
         <FileIcon className={styles.artifactIcon} size={16}/>
         <Link href={teamcityService.homeUrl + artifact.content.href}>{artifact.name}</Link>
-        <span className={styles.bytes}>{prettyBytes(artifact.size)}</span>
+        <span className={styles.bytes}>{fileSize(artifact.size)}</span>
       </span>
     );
   }
