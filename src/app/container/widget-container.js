4cd0af4426774d5a3393d41615bd79ea8fc9b6d9
katriyna <katriyna@gmail.com>
2018-10-02 21:16:47 +0300

    moved to hub-widget-ui 1.0.19

--- src/app/container/widget-container.js
+++ src/app/container/widget-container.js
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {i18n} from 'hub-dashboard-addons/dist/localization';
 
 import Widget from '../widget';
 import {loadArtifacts} from '../redux/actions';
 import ConfigurationContainer from '../configuration/container/configuration-container';
 
-import TitleContainer from './title-container';
 import ContentContainer from './content-container';
+
+const getPresentationalWidgetTitle = state => ({
+  text: state.configuration.title ||
+    (state.buildType
+      ? i18n('Artifacts: {{ buildConfiguration }}', {buildConfiguration: state.buildType.path})
+      : i18n('Artifacts')),
+  counter: -1,
+  href: state.buildType &&
+    state.buildType.id &&
+    `${state.teamcityService.homeUrl}/viewType.html?buildTypeId=${state.buildType.id}`
+});
 
 const WidgetContainer = connect(
   (state, {dashboardApi}) => ({
@@ -15,7 +26,9 @@ const WidgetContainer = connect(
     // eslint-disable-next-line no-magic-numbers
     refreshPeriod: state.refreshPeriod * 1000,
     dashboardApi,
-    Title: TitleContainer,
+    title: state.configuration.isConfiguring
+      ? i18n('Artifacts')
+      : getPresentationalWidgetTitle(state),
     Configuration: ConfigurationContainer,
     Content: ContentContainer
   }),
